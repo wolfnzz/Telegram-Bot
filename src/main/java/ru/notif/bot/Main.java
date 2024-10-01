f3e0611c3323980e5a47716648fa8da467935e74
wolfnzz <147470999+wolfnzz@users.noreply.github.com>
2024-10-01 09:30:00 +0500

    fixed ver

--- src/main/java/ru/notif/bot/Main.java
+++ src/main/java/ru/notif/bot/Main.java
@@ -1,12 +1,17 @@
 package ru.notif.bot;
 
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 import ru.notif.bot.logic.echo_RequestHanger;
 import ru.notif.bot.platforms.console.Console_Bot;
 import ru.notif.bot.platforms.console.Console_InputReader;
 import ru.notif.bot.platforms.console.Console_OutputWriter;
+import ru.notif.bot.platforms.telegrambot.TgBot;
+import ru.notif.bot.platforms.telegrambot.TgBot;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws TelegramApiException {
         Console_InputReader inputReader = new Console_InputReader();
         Console_OutputWriter outputWriter = new Console_OutputWriter();
         echo_RequestHanger requestHandler = new echo_RequestHanger();
@@ -15,5 +20,8 @@ public class Main {
         Console_Bot bot = new Console_Bot(inputReader, requestHandler, outputWriter);
         bot.startBot();
 
+        TgBot bot1 = new TgBot();
+        TelegramBotsApi telegrambot = new TelegramBotsApi(DefaultBotSession.class);
+        telegrambot.registerBot(bot1);
     }
 }
--- src/main/java/ru/notif/bot/platforms/telegrambot/TgBot.java
+++ src/main/java/ru/notif/bot/platforms/telegrambot/TgBot.java
@@ -1,25 +1,127 @@
 package ru.notif.bot.platforms.telegrambot;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.CallbackQuery;
+import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
 
 public class TgBot extends TelegramLongPollingBot{
-    final private String BOT_TOKEN = System.getenv("BOT_TOKEN");
-    final private String BOT_NAME = System.getenv("BOT_NAME");
 
+    final private String token;
+    final private String name;
+
+    public TgBot()
+    {
+        FileInputStream fis;
+        Properties property = new Properties();
+
+        try {
+            fis = new FileInputStream("src/main/resources/bot.properties");
+            property.load(fis);
+
+            token = property.getProperty("BOT_TOKEN");
+            name = property.getProperty("BOT_NAME");
+
+            System.out.println("token: " + token + ", name: " + name);
+
+
+        }
+        catch (IOException e) {
+            System.err.println("ОШИБКА: токен и имя отсуствуют!");
+            throw new IllegalStateException(e);
+        }
+    }
+
+    private void sendWithoutURl(Message message) {
+        InlineKeyboardMarkup keyboard = new InlineKeyboardMarkup();
+        InlineKeyboardButton button = new InlineKeyboardButton();
+
+        button.setText("нажать");
+        button.setCallbackData("start0");
+
+        List<InlineKeyboardButton> keyboardButtonsRow = new ArrayList<InlineKeyboardButton>();
+        keyboardButtonsRow.add(button);
+
+        List<List<InlineKeyboardButton>> rowList = new ArrayList<>();
+        rowList.add(keyboardButtonsRow);
+
+        keyboard.setKeyboard(rowList);
+
+        try{
+            execute(
+                    SendMessage.builder()
+                            .chatId(String.valueOf(message)) // не внедрили переменную в ф-цию
+                            .parseMode("Markdown")
+                            .text("hello world")
+                            .replyMarkup(keyboard)
+                            .build());
+        }
+
+        catch(TelegramApiException e){
+            e.printStackTrace();
+        }
+
+
+    }
     @Override
     public String getBotUsername() {
-        return BOT_NAME;
+        return name;
     }
 
     @Override
     public String getBotToken() {
-        return BOT_TOKEN;
+        return token;
     }
 
     @Override
     public void onUpdateReceived(Update update) {
+        if (update.hasMessage() && update.getMessage().hasText()) {
+            String messageText = update.getMessage().getText();
+            Message command = update.getMessage();
+            long chatID = update.getMessage().getChatId();
+
+            switch (messageText){
+                case "/start":
+                    startAnswer(command);
+                    break;
+                default:
+                    break;
+            }
+
+        }
+        else if (update.hasCallbackQuery()){
+            if (update.getCallbackQuery().getData().equals("start0")){
+                try{
+                    execute(
+                            SendMessage.builder()
+                                    .chatId(String.valueOf(update.getCallbackQuery().getMessage().getChatId()))
+                                    .parseMode("Markdown")
+                                    .text("hello world")
+                                    .build());
+                }
+
+                catch(TelegramApiException e){
+                    e.printStackTrace();
+                }
+            }
+
+        }
     }
 
+    public void startAnswer(Message command){
+        sendWithoutURl(command);
+
+    }
 
 }
